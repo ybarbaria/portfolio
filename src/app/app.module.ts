1f4696174403b37e0858330c434b101741ec05d1
ybarbaria <ybarbaria@gmail>
2018-08-15 19:50:13 +0200

    begin aboutme page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 // Components
 import { AppComponent } from './app.component';
 import { StickyHeaderComponent } from './components/sticky-header.component';
 import { MenuComponent } from './components/menu.component';
+import { AboutMePage } from './pages/aboutme/about-me.page';
 
 @NgModule({
   declarations: [
     AppComponent,
+    // Components
     StickyHeaderComponent,
-    MenuComponent
+    MenuComponent,
+    // Pages
+    AboutMePage
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/aboutme/about-me.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-about-me',
+    templateUrl: './about-me.page.html',
+    styleUrls: ['./about-me.page.scss']
+})
+export class AboutMePage {
+}
